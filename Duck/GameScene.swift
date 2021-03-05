3924d99608734c21f356511349cf5b21e4a1f12a
Randy Thai <randythai360@gmail.com>
2021-03-05 12:36:43 -0600

    You can tap on the nest.

--- Duck/GameScene.swift
+++ Duck/GameScene.swift
@@ -46,12 +46,33 @@ struct PhysicsCategory {
     static let none : UInt32 = 0b100
 }
 
+struct Nests {
+    let loc : CGPoint
+    let name : String
+    let sprite : SKSpriteNode
+    let isOccupied : Bool
+    let nestNumber : Int
+}
+
+struct Buttons {
+    let loc : CGPoint
+    let sprite: SKSpriteNode
+    let isPresent : Bool
+    let parentButton : String
+    let name : String
+}
+
 class GameScene: SKScene, SKPhysicsContactDelegate{
+    
+    //Unique Duck Identifier
     var duckIDX = 0
+    //Unique Nest Identifier
+    var nestIDX = 0
+    var buttonIDX = 0
     
     var currentMap = SKSpriteNode(imageNamed: "TestMap")
     var portal = SKSpriteNode(imageNamed:"portal")
-
+    
     public var remainingLives = 10
     public var healthLabel = SKLabelNode()
     public var currentMoney = 100
@@ -60,15 +81,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     var duckCost = 100
     var gooseReward = 50
     
-    //Stores Information on Ducks
+    
+    //Stores Information on Ducks and their corresponding detection radiuses in an array
+    //Stored in a swift lock-key system
     var duckLocs = [(SKSpriteNode,SKShapeNode)]()
-    //Stores Information about Detection Circles
-    var detectionLocs: [SKShapeNode] = []
+    
+    //An array of all the buttons CURRENTLY on the screen
+    var currentButtons: [Buttons] = []
+    
+    //An array of all the nests CURRENTLY on the screen
+    var currentNests: [Nests] = []
+    
+    
+    /* -------------------- FUNCTIONS -------------------- */
+    
+    func random() -> CGFloat {
+      return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+      return random() * (max - min) + min
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
     
     override func didMove(to view: SKView) {
         
         physicsWorld.contactDelegate = self
 
+        //Adding nest to appropriate spots
+        addNest(location: CGPoint(x: self.frame.width/2 - 358, y: self.frame.height/2 - 150))
+        
         //Label for the lives remaining
         healthLabel.text = "Remaining Lives:  " + String(remainingLives)
         healthLabel.zPosition = 2
@@ -76,7 +121,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         healthLabel.fontSize = CGFloat(20)
         healthLabel.fontColor = .black
         addChild(healthLabel)
-        
         
         //Label for current money supply
         moneyLabel.text = "$: " + String(currentMoney)
@@ -99,22 +143,116 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         portal.size = CGSize(width: 100, height: 110)
         
         addChild(portal)
-        
  
         run(SKAction.repeat(SKAction.sequence([SKAction.run(addGoose), SKAction.wait(forDuration: 2.5)]), count: 10))
 
-        
         }
+    
+    //Detects any COLLISIONS and CONTACTS
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if (firstBody.categoryBitMask == PhysicsCategory.enemy) && (secondBody.categoryBitMask == PhysicsCategory.detection) {
+            detectionHandler(obj: secondBody.node as! SKShapeNode, thing: firstBody.node as! SKSpriteNode)
+        }else if (secondBody.categoryBitMask == PhysicsCategory.enemy) && (firstBody.categoryBitMask == PhysicsCategory.detection) {
+            detectionHandler(obj: firstBody.node as! SKShapeNode, thing: secondBody.node as! SKSpriteNode)
+        }else if (firstBody.categoryBitMask == PhysicsCategory.enemy) && (secondBody.categoryBitMask == PhysicsCategory.projectile) {
+            collisionHandler(proj: secondBody.node as! SKSpriteNode, enemy: firstBody.node as! SKSpriteNode)
+        }else if (secondBody.categoryBitMask == PhysicsCategory.enemy) && (firstBody.categoryBitMask == PhysicsCategory.projectile) {
+            collisionHandler(proj: firstBody.node as! SKSpriteNode, enemy: secondBody.node as! SKSpriteNode)
+        }else{
+            return
+        }
+        
+    }
+    
+    func showOptions(location : CGPoint, nn: String) {
+        var idx = 0
+        let posScale: CGFloat = 60 //Lower the number, lower the position
+        let heightScale: CGFloat = 25 //Lower the number, the bigger the sprite
+        var createdButton = false //Checks whether or not this is the first time you create the button
+        if(currentButtons.count <= 0) {
+            let buttonInfo = Buttons(loc: location, sprite: SKSpriteNode(imageNamed: "Breadcrumb"), isPresent: true, parentButton: nn, name: "Button\(buttonIDX)")
+            
+            let button = buttonInfo.sprite
+            button.position = CGPoint(x: buttonInfo.loc.x, y: buttonInfo.loc.y + posScale)
+            button.size = CGSize(width: button.size.width/(self.frame.width/heightScale), height: button.size.height/(self.frame.width/heightScale))
+            button.zPosition = 3
+            button.alpha = 0.9
+        
+            buttonIDX+=1
+            currentButtons.append(buttonInfo)
+            addChild(button)
+            createdButton = true
+        }
+        for button in currentButtons {
+            if (button.parentButton == nn && button.isPresent == false) && (createdButton == false){
+                print(1)
+                
+                let buttonInfo = Buttons(loc: location, sprite: SKSpriteNode(imageNamed: "Breadcrumb"), isPresent: true, parentButton: nn, name: "Button\(buttonIDX)")
+                
+                let button = buttonInfo.sprite
+                button.position = CGPoint(x: buttonInfo.loc.x, y: buttonInfo.loc.y + posScale)
+                button.size = CGSize(width: button.size.width/(self.frame.width/heightScale), height: button.size.height/(self.frame.width/heightScale))
+                button.zPosition = 3
+                button.alpha = 0.9
+                button.name = buttonInfo.name
+            
+                buttonIDX+=1
+                currentButtons.append(buttonInfo)
+                addChild(button)
+                
+                return
+            } else if button.isPresent && createdButton == false {
+                print("falsed")
+                currentButtons.remove(at: idx)
+                button.sprite.removeFromParent()
+                return
+            }
+            idx+=1
+        }
+        
+        
+    }
+    
+    //Make a button function:
+    /*
+     let buttonInfo = Buttons(loc: location, sprite: SKSpriteNode(imageNamed: "Breadcrumb"), didPress: true, parentButton: nn, name: "Button\(buttonIDX)")
      
-    //Touch recognition
+     let button = buttonInfo.sprite
+     button.position = buttonInfo.loc
+     button.size = CGSize(width: button.size.width/(self.frame.width/20), height: button.size.height/(self.frame.width/20))
+     button.zPosition = 3
+     button.alpha = 0.9
+ 
+     buttonIDX+=1
+     currentButtons.append(buttonInfo)
+     addChild(button)
+     */
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             
             let location = touch.location(in: self)
-            addDuck(loc: location)
             
-            //Adds a duck to the location where you tapped (temporary).
-            
+            //For buttons
+            enumerateChildNodes(withName: "//*", using: { (node, stop) in
+                
+                if node.name?.prefix(4) == "Nest" {
+                    
+                    if node.contains(location){
+                        node.alpha = 0.5
+                        self.showOptions(location: node.position, nn: node.name!)
+                    }
+                    
+                } else if node.name?.prefix(6) == "Button" {
+                    if node.contains(location) {
+                        node.alpha = 0.5
+                    }
+                }
+                
+            })
             
             //Checks if there is a duck at where you tapped.
             for duck in duckLocs {
@@ -131,21 +269,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         }
     }
     
-    func random() -> CGFloat {
-      return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            enumerateChildNodes(withName: "//*", using: { (node, stop) in
+                
+                if node.name?.prefix(4) == "Nest" {
+                    if node.contains(touch.location(in: self)){
+                        node.alpha = 1
+                    }
+                } else if node.name?.prefix(5) == "Button" {
+                    if node.contains(touch.location(in: self)){
+                        node.alpha = 1
+                    }
+                }
+                
+            })
+        }
     }
-
-    func random(min: CGFloat, max: CGFloat) -> CGFloat {
-      return random() * (max - min) + min
+    
+    /* -------------------- ADD FUNCTIONS -------------------- */
+ 
+    func addNest(location: CGPoint) {
+        //Store Information in the struct
+        let nestInfo = Nests(loc: location, name: "Nest\(nestIDX)", sprite: SKSpriteNode(imageNamed: "Nest"), isOccupied: false, nestNumber: nestIDX)
+        currentNests.append(nestInfo)
+        
+        //Make the actual nest
+        let nest = nestInfo.sprite
+        nest.position = nestInfo.loc
+        nest.name = nestInfo.name
+        nest.size = CGSize(width: nest.size.width/(self.frame.width/45), height: nest.size.height/(self.frame.width/45))
+        nest.zPosition = 2
+        
+        
+        addChild(nest)
+        nestIDX += 1
     }
     
     func addDuck(loc: CGPoint) {
         physicsWorld.contactDelegate = self
-        if(duckIDX >= 5){ //Max amount of Ducks = 5
-            return
-        }
-        //Only allows a duck to be placed if player has enough money, and subtracts that money from their total
-        if (self.currentMoney >= duckCost) {
+        
+        //Only allows a duck to be placed if player has enough money and if there is not more than 5 ducks, and subtracts that money from their total
+        if (self.currentMoney >= duckCost && duckIDX < 5) {
             self.currentMoney -= duckCost
             self.moneyLabel.text = "$: " + String(self.currentMoney)
         } else {
@@ -153,15 +318,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         }
         //Makes a duck
         let duck = SKSpriteNode(imageNamed: "BasicDuckFullBody")
+        duckIDX+=1
         duck.position = loc
         duck.size = CGSize(width: 100, height: 110)
         duck.name = "Duck\(duckIDX)"
-        duck.zPosition = 1
-        duckIDX+=1
+        duck.zPosition = 3
         
         // Detection Circle to detect Geese that are close
         let detectionCircle = SKShapeNode(circleOfRadius: 100)
-        detectionCircle.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        detectionCircle.physicsBody = SKPhysicsBody(circleOfRadius: 80)
         detectionCircle.position = CGPoint(x: duck.position.x - 2, y: duck.position.y + 15)
         detectionCircle.fillColor = .cyan
         detectionCircle.physicsBody?.affectedByGravity = false
@@ -197,14 +362,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         goose.physicsBody?.categoryBitMask = PhysicsCategory.enemy
         goose.physicsBody?.collisionBitMask = PhysicsCategory.projectile
         goose.physicsBody?.contactTestBitMask = PhysicsCategory.detection | PhysicsCategory.projectile
-        
-        
-        
-      // Determine where to spawn the monster along the Y axis
-      //let actualY = random(min: goose.size.height/2, max: size.height - goose.size.height/2)
-      
-      // Position the monster slightly off-screen along the right edge,
-      // and along a random position along the Y axis as calculated above
 
         goose.position = CGPoint(x: self.frame.width/8.75, y: self.frame.height/1.05)
       
@@ -221,25 +378,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             SKAction.run {goose.zRotation = CGFloat(Double.pi/2.0)}
         ])
             
-           
-        
         let secondMove = SKAction.sequence([
             SKAction.move(to: CGPoint(x: self.frame.width/3.3, y: self.frame.height/2.82), duration: TimeInterval((250.0/250.0)/gooseSpeed)),
             SKAction.run {goose.zRotation = CGFloat(Double.pi)}])
-        
         
         let thirdMove = SKAction.sequence([
             SKAction.move(to: CGPoint(x: self.frame.width/3.3, y: self.frame.height/1.30), duration: TimeInterval((245.0/250.0)/gooseSpeed)),
             SKAction.run {goose.zRotation = CGFloat(Double.pi/2.0)}])
         
-        
         let fourthMove = SKAction.sequence([
                                             SKAction.move(to: CGPoint(x: self.frame.width/1.08, y: self.frame.height/1.30), duration: TimeInterval((685.0/250.0)/gooseSpeed)),
             SKAction.run {goose.zRotation = CGFloat(Double.pi * 0)}])
         
-        
         let fifthMove = SKAction.move(to: CGPoint(x: self.frame.width/1.08, y: self.frame.height/5), duration: TimeInterval((320.0/250.0)/gooseSpeed))
-        
         
         let finalAction = SKAction.sequence(
             [SKAction.run {self.remainingLives -= 1},
@@ -250,22 +401,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
     }
     
-    //Used for the detection circle to indicate whether or not a goose has entered the "bread" zone
-    //NOTE: This is where you can get the geese's position as well ("thing" is the reference to the goose)
-    func detectionHandler(obj: SKShapeNode, thing: SKSpriteNode){
-        thing.removeFromParent()
-        self.currentMoney += gooseReward
-        self.moneyLabel.text = "$: " + String(self.currentMoney)
-        launchBreadcrumb(startPoint: obj.position, endPoint: thing.position)
-    }
-    
-    //Used to actually deal damage to the goose if the breadcrumbs collide with a goose.
-    func collisionHandler(proj: SKSpriteNode, enemy: SKSpriteNode){
-        enemy.removeFromParent()
-        self.currentMoney += gooseReward
-        self.moneyLabel.text = "$: " + String(self.currentMoney)
-    }
-    
+    /* -------------------- ACTIONS -------------------- */
     func launchBreadcrumb (startPoint: CGPoint, endPoint: CGPoint) {
         let crumb = SKSpriteNode (imageNamed: "Breadcrumb")
         crumb.size = CGSize(width: 30, height: 30)
@@ -289,25 +425,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         crumb.run(SKAction.sequence([SKAction.move(to: endPoint, duration: 0.2), SKAction.removeFromParent()]))
     }
     
-    func didBegin(_ contact: SKPhysicsContact) {
-        let firstBody = contact.bodyA
-        let secondBody = contact.bodyB
-        
-        if (firstBody.categoryBitMask == PhysicsCategory.enemy) && (secondBody.categoryBitMask == PhysicsCategory.detection) {
-            detectionHandler(obj: secondBody.node as! SKShapeNode, thing: firstBody.node as! SKSpriteNode)
-        }else if (secondBody.categoryBitMask == PhysicsCategory.enemy) && (firstBody.categoryBitMask == PhysicsCategory.detection) {
-            detectionHandler(obj: firstBody.node as! SKShapeNode, thing: secondBody.node as! SKSpriteNode)
-        }else if (firstBody.categoryBitMask == PhysicsCategory.enemy) && (secondBody.categoryBitMask == PhysicsCategory.projectile) {
-            collisionHandler(proj: secondBody.node as! SKSpriteNode, enemy: firstBody.node as! SKSpriteNode)
-        }else if (secondBody.categoryBitMask == PhysicsCategory.enemy) && (firstBody.categoryBitMask == PhysicsCategory.projectile) {
-            collisionHandler(proj: firstBody.node as! SKSpriteNode, enemy: secondBody.node as! SKSpriteNode)
-        }else{
-            return
-        }
-        
+    /* -------------------- HANDLERS -------------------- */
+    
+    //Used for the detection circle to indicate whether or not a goose has entered the "bread" zone
+    //NOTE: This is where you can get the geese's position as well ("thing" is the reference to the goose)
+    func detectionHandler(obj: SKShapeNode, thing: SKSpriteNode){
+        thing.removeFromParent()
+        self.currentMoney += gooseReward
+        self.moneyLabel.text = "$: " + String(self.currentMoney)
+        launchBreadcrumb(startPoint: obj.position, endPoint: thing.position)
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    //Used to actually deal damage to the goose if the breadcrumbs collide with a goose.
+    func collisionHandler(proj: SKSpriteNode, enemy: SKSpriteNode){
+        enemy.removeFromParent()
+        self.currentMoney += gooseReward
+        self.moneyLabel.text = "$: " + String(self.currentMoney)
     }
+    
 }
