df19288e8b371f92a90ff453f14e8f6d2caeca1b
Randy Thai <randythai360@gmail.com>
2021-04-14 12:23:24 -0500

    Rescaling of scenes works fine now. Hard-coded start menu button for more customizability.

--- Duck/MainMenuController.swift
+++ Duck/MainMenuController.swift
@@ -17,10 +17,23 @@ class MainMenuController: UIViewController {
 
         MainMenuArt.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         view.addSubview(MainMenuArt)
+        
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: self.view.frame.size.width/2 - 70, y: self.view.frame.size.height/2 + 80, width: 130, height: 110)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.clipsToBounds = true
+        button.backgroundColor = .systemRed
+        button.setTitle("START", for: .normal)
+        button.addTarget(self, action: #selector(startButtonPressed), for: .touchUpInside)
+        view.addSubview(button)
+        
+        print("BUTTON")
         // Do any additional setup after loading the view.
     }
     
-    
+    @objc func startButtonPressed() {
+        performSegue(withIdentifier: "toGameScene", sender: nil)
+    }
     
     /*
     // MARK: - Navigation
